08da130c0c63ae841ffd0239f45e9b0e14d6bfed
Shadreck kasera <shadkasera@gmail.com>
2023-04-25 14:03:28 +0200

    initial commit

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -8,45 +8,79 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[400],
       body: SafeArea(
-          child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 30,
-              horizontal: 50,
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.orange,
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.circular(2),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 30,
+                horizontal: 50,
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:  [
-                  Image.asset('assets/images/notes-removebg-preview.png',height: 50,width: 50,color: Colors.white,),
-                  // const Padding(
-                  //   padding: EdgeInsets.all(20.0),
-                  //   child: Icon(
-                  //     Icons.add_task,
-                  //     color: Colors.white,
-                  //     size: 40,
-                  //   ),
-                  // ),
-                  const Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                      'Tasks',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(2),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/notes-removebg-preview.png',
+                      height: 50,
+                      width: 50,
+                      color: Colors.white,
                     ),
-                  ),
-                ],
+                    const Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        'Tasks',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          //another container
-        ],
-      )),
+            //another container
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 30,
+                horizontal: 50,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(2),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/one.png',
+                      height: 50,
+                      width: 50,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        'Reminders',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
