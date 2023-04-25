fca84354ee89a1bcb7b38638f31e76f5f87d6429
Shadreck kasera <shadkasera@gmail.com>
2023-04-25 16:46:32 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ), debugShowCheckedModeBanner: false,
       
-      home: const StopWatch(),
+      home: const WelcomePage(),
     );
   }
 }
